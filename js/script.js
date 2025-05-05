79dda133879f621ef2b29062bc07c99e55fde24b
Nicolebalzano <nicole.balzano03@gmail.com>
2025-05-05 17:23:12 +0200

    feat:snack bonus done

--- js/script.js
+++ js/script.js
@@ -37,54 +37,86 @@
  
 //  }
 //  console.log(minWeight);
-let min = 1;
-let max = 30;
-let finalValues = [];
+
 
 // SNACK 2
-const teams = [
-    {
-        nome : "Napoli",
-        puntiFatti : 0, 
-        falliSubiti : 0,
-    },
-    {
-        nome :"Torino",
-        puntiFatti : 0,
-        falliSubiti : 0,
-    },
-    {
-        nome :"Juventus",
-        puntiFatti : 0,
-        falliSubiti : 0,
-    },
-    {
-        nome :"Roma",
-        puntiFatti : 0,
-        falliSubiti : 0,
-    },
-    {
-        nome :"Milan",
-        puntiFatti : 0,
-        falliSubiti : 0,
-    }
-]
-// variabile che genera il numero random
-    let random = (Math.floor(Math.random() * (max - min + 1) + min));
-    for(let i = 0; i < teams.length; i++){
-    teams[i].puntiFatti = random;
-        teams[i].falliSubiti = random;
-        }
-        console.log(teams);
+// let min = 1;
+// let max = 30;
+// let finalValues = [];
+
+
+// const teams = [
+//     {
+//         nome : "Napoli",
+//         puntiFatti : 0, 
+//         falliSubiti : 0,
+//     },
+//     {
+//         nome :"Torino",
+//         puntiFatti : 0,
+//         falliSubiti : 0,
+//     },
+//     {
+//         nome :"Juventus",
+//         puntiFatti : 0,
+//         falliSubiti : 0,
+//     },
+//     {
+//         nome :"Roma",
+//         puntiFatti : 0,
+//         falliSubiti : 0,
+//     },
+//     {
+//         nome :"Milan",
+//         puntiFatti : 0,
+//         falliSubiti : 0,
+//     }
+// ]
+// // variabile che genera il numero random
+//     let random = (Math.floor(Math.random() * (max - min + 1) + min));
+//     for(let i = 0; i < teams.length; i++){
+//     teams[i].puntiFatti = random;
+//         teams[i].falliSubiti = random;
+//         }
+//         console.log(teams);
         
     
 
-    for (let i = 0; i < teams.length; i++) {
-        let curFalli = teams[i].puntiFatti;
-      let teamNome = teams[i].nome;
-      finalValues.push({falliSubiti : curFalli, teamName : teamNome});
-    }
-    console.log(finalValues);
+//     for (let i = 0; i < teams.length; i++) {
+//         let curFalli = teams[i].puntiFatti;
+//       let teamNome = teams[i].nome;
+//       finalValues.push({falliSubiti : curFalli, teamName : teamNome});
+//     }
+//     console.log(finalValues);
+
+
+// Snack 3 (Bonus)
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri. Non usare i metodi di array di JS
+let finalNumbers = [];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let a = 3;
+let b = 7;
+
+numeriCompresi(a, b, numbers);
+function numeriCompresi (a, b, numbers){
+    for (let i = 0; i < numbers.length; i++) {
+    let curNum = numbers[i];
+        if(curNum >= a && curNum <= b){
+            finalNumbers.push(curNum);
+        }
+   
+    } return finalNumbers;
+}
+console.log(finalNumbers);
+
+
+
+
+
+
+
+
     
 
     
