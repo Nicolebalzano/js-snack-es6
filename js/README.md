3970d231a181e9e3321e3fcf37898c8acfe6dd9a
Nicolebalzano <nicole.balzano03@gmail.com>
2025-05-05 16:43:30 +0200

    feat:snack 2 completed;

--- js/README.md
+++ js/README.md
@@ -15,6 +15,6 @@ SNACK 2
 --> nome, punti fatti, falli subiti
 - La proprietà nome deve essere compilata.
 punti fatti e falli subiti = 0; 
-- quindi creo due funzioni che mi generano dei numeri random per le precedenti proprietà settate a 0; 
+- quindi creo una funzione che mi generano dei numeri random per le precedenti proprietà settate a 0; 
 - Il risultato dei falli e i nomi della squadra devono poi essere pushati in un nuovo array che li stampa o in log o in pagina
 
--- js/script.js
+++ js/script.js
@@ -1,43 +1,91 @@
 // SNACK 1
-const weight = [];
-const bikes = [
+// const weight = [];
+// const bikes = [
+//     {
+//         nome : "superBike",
+//         peso : 13 ,
+//     },
+//     {
+//         nome : "bike2000",
+//         peso : 11 ,
+//     },
+//     {
+//         nome : "theFaster",
+//         peso : 20 ,
+//     },
+//     {
+//         nome : "flyBike",
+//         peso : 17,
+//     },
+//     {
+//         nome : "theGreatest",
+//         peso : 25 ,
+//     }
+// ]
+
+// for (let i = 0; i < bikes.length; i++){
+// let bikePeso = bikes[i].peso; 
+//  weight.push(bikePeso);
+
+// }
+// let minWeight = weight[0];
+//  for(let i = 0; i < weight.length; i++){
+//     let curWeight = weight[i];
+//     if(curWeight <= minWeight){
+//         minWeight = curWeight;
+//     }
+ 
+//  }
+//  console.log(minWeight);
+let min = 1;
+let max = 30;
+let finalValues = [];
+
+// SNACK 2
+const teams = [
     {
-        nome : "superBike",
-        peso : 13 ,
+        nome : "Napoli",
+        puntiFatti : 0, 
+        falliSubiti : 0,
     },
     {
-        nome : "bike2000",
-        peso : 11 ,
+        nome :"Torino",
+        puntiFatti : 0,
+        falliSubiti : 0,
     },
     {
-        nome : "theFaster",
-        peso : 20 ,
+        nome :"Juventus",
+        puntiFatti : 0,
+        falliSubiti : 0,
     },
     {
-        nome : "flyBike",
-        peso : 17,
+        nome :"Roma",
+        puntiFatti : 0,
+        falliSubiti : 0,
     },
     {
-        nome : "theGreatest",
-        peso : 25 ,
+        nome :"Milan",
+        puntiFatti : 0,
+        falliSubiti : 0,
     }
 ]
-
-for (let i = 0; i < bikes.length; i++){
-let bikePeso = bikes[i].peso; 
- weight.push(bikePeso);
-
-}
-let minWeight = weight[0];
- for(let i = 0; i < weight.length; i++){
-    let curWeight = weight[i];
-    if(curWeight < minWeight){
-        minWeight = curWeight;
+// variabile che genera il numero random
+    let random = (Math.floor(Math.random() * (max - min + 1) + min));
+    console.log(random);
+    
+    for (let i = 0; i < teams.length; i++) {
+        let curFalli = random;
+      let teamNome = teams[i].nome;
+      finalValues.push({falliSubiti : curFalli, teamName : teamNome});
     }
- 
- }
- console.log(minWeight);
- 
- 
+    console.log(finalValues);
+    
+
+    
+    
+
+
+
+
 
 
